b24bee571d6b23a23b0367e820a84f1ee96fea5f
SahanAtNeura <sahan@neuraintelligence.com>
2023-12-21 06:07:52 +0100

    New Pre Hook Test 3

--- tests/app/user/services/test_user.py
+++ tests/app/user/services/test_user.py
@@ -5,4 +5,4 @@ from app.user.services.user import UserService
 
 @pytest.mark.asyncio
 async def test_is_admin():
-    assert await UserService().is_admin() is True
+    assert await UserService().is_admin()
